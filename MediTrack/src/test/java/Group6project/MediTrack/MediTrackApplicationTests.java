e32b732317466dc7215bd77b8630f927b87e80e5
Marshall Welch <mjw.nkg@gmail.com>
2026-02-23 21:35:09 -0800

    first commit

---
+++ MediTrack/src/test/java/Group6project/MediTrack/MediTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package Group6project.MediTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
